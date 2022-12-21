82a4a51ef45dbc57dcea3e341e813bd83166a659
dev ops <devops@agile.com.np>
2022-12-21 21:46:53 +0545

    third commit removed email and password

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -128,6 +128,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'aman.1si17cs008@gmail.com'
-EMAIL_HOST_PASSWORD = 'rpeicjmknwcpzqka'
+EMAIL_HOST_USER = 'your Email'
+EMAIL_HOST_PASSWORD = 'your password'
 
--- ToDolistapp/views.py
+++ ToDolistapp/views.py
@@ -108,7 +108,7 @@ def password_reset_request(request):
 					}
 					email = render_to_string(email_template_name, c)
 					try:
-						send_mail(subject, email, 'aman.1si17cs008@gmail.com' , [user.email], fail_silently=False)
+						send_mail(subject, email, 'YourEmail' , [user.email], fail_silently=False)
 					except BadHeaderError:
 						return HttpResponse('Invalid header found.')
 					return redirect ("password_reset_done")
